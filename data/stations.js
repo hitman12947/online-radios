436ef6f62baba775b45fa90d1854b0f9a503d4b7
Michelle Fèvre <rubi_81@freenet.de>
2020-12-08 18:10:42 +0100

    Update stations.js
    add more radio stations

--- data/stations.js
+++ data/stations.js
@@ -7,4 +7,20 @@ export default [
     name: 'BBC Media',
     url: 'http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio1_mf_p',
   },
+  {
+    name: 'Antenne Bayern',
+    url: 'http://mp3channels.webradio.antenne.de/80er-kulthits',
+  },
+  {
+    name: 'SWR 1',
+    url: 'http://mp3-live.swr.de/swr1rp_m.m3u',
+  },
+  {
+    name: 'BBC 1',
+    url: 'http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio1_mf_p',
+  },
+  {
+    name: 'Radio France',
+    url: 'http://direct.fipradio.fr/live/fip-midfi.mp3',
+  }
 ];
